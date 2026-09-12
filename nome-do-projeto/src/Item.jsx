29785f3b15136bf0120e515fe82cf5eafcdca5e8
Luiz Eduardo Baggio Spanhol <luizeduardo3190@gmail.com>
2026-09-12 15:34:45 -0300

    Adicionando um botão para remover um item da lista

--- nome-do-projeto/src/Item.jsx
+++ nome-do-projeto/src/Item.jsx
@@ -1,19 +1,32 @@
 import { useState } from "react";
 
 export function Item() {
-  const [produtos] = useState([
+  const [produtos, setProdutos] = useState([
     { nome: "Pão", preco: 10 },
     { nome: "Leite", preco: 8 },
     { nome: "Bolacha", preco: 7 },
   ]);
 
+  function removerProduto(nomeProduto) {
+    const novaLista = produtos.filter(
+      (produto) => produto.nome !== nomeProduto,
+    );
+    setProdutos(novaLista);
+  }
+
   return (
     <div>
       {produtos.map((produto, index) => {
         return (
-          <p key={`${index}-${produto.nome}`}>
-            {produto.nome} - {produto.preco}
-          </p>
+          <div>
+            <p key={`${index}-${produto.nome}`}>
+              {produto.nome} - {produto.preco}
+            </p>
+
+            <button onClick={() => removerProduto(produto.nome)}>
+              Remover Item
+            </button>
+          </div>
         );
       })}
     </div>
